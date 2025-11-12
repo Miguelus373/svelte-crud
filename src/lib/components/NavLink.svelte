03a6fbac3f9a0f99d6a64460cb92a707f7278a46
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2025-11-11 21:57:43 -0400

    Add navlink to layout

---
+++ src/lib/components/NavLink.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { page } from "$app/state";
+  export let href;
+
+  $: isActive = page.url.pathname == href;
+</script>
+
+<a href={href}>
+  <slot />
+</a>
+
+<style>
+  a {
+    font-weight: 600;
+    font-size: 1rem;
+    padding: 1rem;
+    color: azure;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,23 @@
 <script>
 	import '../app.css';
+	import { page } from '$app/stores';
+	import NavLink from '$lib/components/NavLink.svelte';
 	
 	let { children } = $props();
 </script>
 
+<nav>
+	<NavLink href="/">Inicio</NavLink>
+	<NavLink href="/publicaciones">Publicaciones</NavLink>
+</nav>
+
 {@render children()}
+
+<style>
+	nav {
+		padding: 1rem 1rem;
+		background-color: teal;
+		display: flex;
+		justify-content: center;
+	}
+</style>
