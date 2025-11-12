9eb3af75f2dce444727c5d681602ecfa006e9c4f
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2025-11-11 21:58:04 -0400

    Add articles

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,63 @@
 // place files you want to import through the `$lib` alias in this folder.
+export const publicaciones = [
+	{
+		title: 'Understanding JavaScript Closures',
+		author: 'Jane Doe',
+		slug: 'understanding-javascript-closures'
+	},
+	{
+		title: 'A Guide to Flexbox in CSS',
+		author: 'John Smith',
+		slug: 'a-guide-to-flexbox-in-css'
+	},
+	{
+		title: 'Async/Await in Modern JavaScript',
+		author: 'Alice Lee',
+		slug: 'async-await-in-modern-javascript'
+	},
+	{
+		title: 'Responsive Web Design Basics',
+		author: 'Bob Brown',
+		slug: 'responsive-web-design-basics'
+	},
+	{
+		title: 'Introduction to React Hooks',
+		author: 'Carol White',
+		slug: 'introduction-to-react-hooks'
+	},
+	{
+		title: 'Mastering Git and GitHub',
+		author: 'David Green',
+		slug: 'mastering-git-and-github'
+	},
+	{
+		title: 'CSS Grid Layout Crash Course',
+		author: 'Eva Black',
+		slug: 'css-grid-layout-crash-course'
+	},
+	{
+		title: 'Node.js Streams Explained',
+		author: 'Frank Blue',
+		slug: 'node-js-streams-explained'
+	},
+	{
+		title: 'TypeScript for Beginners',
+		author: 'Grace Red',
+		slug: 'typescript-for-beginners'
+	},
+	{
+		title: 'Building REST APIs with Express',
+		author: 'Henry Gold',
+		slug: 'building-rest-apis-with-express'
+	},
+	{
+		title: '7 Tips To Write Clean And Better Code in 2025',
+		author: 'GeeksforGeeks',
+		slug: '7-tips-to-write-clean-and-better-code-in-2025'
+	},
+	{
+		title: 'The Six Commandments of Good Code',
+		author: 'Zachary Goldberg',
+		slug: 'the-six-commandments-of-good-code'
+	}
+];
---
+++ src/routes/publicaciones/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { publicaciones } from '$lib';
+	import Publicacion from '$lib/components/Publicacion.svelte';
+</script>
+
+<div class="d-flex">
+  {#each publicaciones as p}
+    <Publicacion slug={p.slug} title={p.title} author={p.author} />
+  {/each}
+</div>
+
+<style>
+  div {
+    display: grid;
+    grid-template-rows: 8rem 8rem;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 1rem;
+    margin: 2rem auto 0;
+    width: 75vw;
+  }
+</style>
