7ba6534ec2aea37d99b69801f415dda39a4f2279
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2025-11-11 22:08:08 -0400

    Add lorem component

---
+++ src/lib/components/Lorem.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let count = 1;
+
+  const lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, iure iste iusto animi tempore quibusdam deserunt saepe vitae at eos voluptatum ut illo tempora cupiditate aperiam quo necessitatibus!';
+</script>
+
+<p>
+  {#each {length: count}}
+    {lorem}
+  {/each}
+</p>
+
+<style>
+  p {
+    padding: 1rem 0;
+    text-align: justify;
+  }
+</style>
--- src/routes/publicaciones/[slug]/+page.svelte
+++ src/routes/publicaciones/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import { publicaciones } from '$lib';
   import { page } from '$app/stores';
+	import Lorem from '$lib/components/Lorem.svelte';
 
   $: slug = $page.params.slug;
 
@@ -12,13 +13,12 @@
 
 <div>
   <a href="/publicaciones">Volver</a> 
-  <div class="img"></div>
 
   <h1>{title}</h1>
   <span>{author}</span>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere assumenda reiciendis quo dolore suscipit inventore nesciunt architecto nobis hic nulla voluptates repellendus in animi rerum, cumque numquam doloremque, excepturi maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere assumenda reiciendis quo dolore suscipit inventore nesciunt architecto nobis hic nulla voluptates repellendus in animi rerum, cumque numquam doloremque, excepturi maxime?</p>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere assumenda reiciendis quo dolore suscipit inventore nesciunt architecto nobis hic nulla voluptates repellendus in animi rerum, cumque numquam doloremque, excepturi maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere assumenda reiciendis quo dolore suscipit inventore nesciunt architecto nobis hic nulla voluptates repellendus in animi rerum, cumque numquam doloremque, excepturi maxime?</p>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere assumenda reiciendis quo dolore suscipit inventore nesciunt architecto nobis hic nulla voluptates repellendus in animi rerum, cumque numquam doloremque, excepturi maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere assumenda reiciendis quo dolore suscipit inventore nesciunt architecto nobis hic nulla voluptates repellendus in animi rerum, cumque numquam doloremque, excepturi maxime?</p>
+  <Lorem count={4} />
+  <Lorem count={2} />
+  <Lorem count={3} />
 </div>
 
 <style>
@@ -30,10 +30,6 @@
   h1 {
     font-size: 2rem;
     margin-bottom: 10px;
-  }
-
-  p {
-    padding: 1rem 0;
   }
 
   a {
