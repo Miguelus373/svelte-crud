bdcb0c37dd8825ba3dbd720f845d9fcb99444598
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2025-11-11 21:57:23 -0400

    Archivos base

---
+++ src/lib/components/Publicacion.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let title;
+  export let author;
+  export let slug;
+</script>
+
+<a href={`/publicaciones/${slug}`}>
+  <h1>{title}</h1>
+  <span>Autor: {author}</span>
+</a>
+
+<style>
+  a {
+    display: inline-block;
+    width: 20rem;
+    padding: 1rem 1.5rem;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    border: 2px solid teal;
+    min-height: 8rem;
+    border-radius: 8px;
+    background-color: rgb(182, 248, 226);
+  }
+
+  a:hover {
+    cursor: pointer;
+    background-color: rgb(67, 253, 191);
+    box-shadow: 3px 4px 15px gray;
+    transition: all .3s;
+  }
+  
+  h1 {
+    font-size: 22px;
+    margin-bottom: 8px;
+  }
+
+  span {
+    font-size: 14px;
+  }
+</style>
