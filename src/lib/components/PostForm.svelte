2e0b31259a860aac75df6398ef1dea57fa1275a7
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2025-11-28 14:07:35 -0400

    Add new post page

---
+++ src/lib/components/PostForm.svelte
@@ -0,0 +1,59 @@
+<script>
+	export let nombre = '';
+	export let cuerpo = '';
+  export let title = '';
+  export let action = '';
+</script>
+
+<form action={action}>
+  <h1>{title}</h1>
+
+  <label for="nombre">Nombre de la publicaión</label>
+  <input type="text" name="nombre" bind:value={nombre} placeholder="Nombre" />
+
+  <label for="nombre">Cuerpo de la publicaión</label>
+  <textarea
+    type="text"
+    name="cuerpo"
+    bind:value={cuerpo}
+    placeholder="Ingrese aqui el cuerpo de la publicación"
+    rows={6}
+  ></textarea>
+
+  <button type="submit">Enviar</button>
+</form>
+
+<style>
+	form {
+		width: 100%;
+	}
+
+	h1 {
+		font-size: 1.5rem;
+		margin: 10px 0;
+	}
+
+	label {
+		display: block;
+		margin-bottom: 10px;
+	}
+
+	input,
+	textarea {
+		border-radius: 12px;
+		border: 1px solid grey;
+		padding: 4px 12px;
+		margin-bottom: 14px;
+		width: 100%;
+		display: block;
+	}
+
+  button {
+    width: 100%;
+    border-radius: 8px;
+    padding: 6px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+  }
+</style>
---
+++ src/routes/publicaciones/nueva/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import PostForm from "$lib/components/PostForm.svelte";
+</script>
+
+<div>
+	<a href="/publicaciones">Volver</a>
+
+	<PostForm title="Crear Publicaión" />
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 28rem;
+		margin: 3rem auto;
+	}
+
+	a {
+		color: rgb(0, 87, 87);
+		width: 100%;
+	}
+</style>
